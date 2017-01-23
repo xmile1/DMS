4a66b36aef331f760c21deb78b883af4efcdab4f
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2017-01-23 09:41:31 +0100

    feature(): implement login and signup function [Finishes #137645875]

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # document-management-system
+[![Build Status](https://travis-ci.org/andela-uenabulele/document-management-system.svg?branch=master)](https://travis-ci.org/andela-uenabulele/document-management-system)
+[![Coverage Status](https://coveralls.io/repos/github/andela-uenabulele/document-management-system/badge.svg?branch=master)](https://coveralls.io/github/andela-uenabulele/document-management-system?branch=master)
+[![Code Climate](https://codeclimate.com/github/andela-uenabulele/document-management-system/badges/gpa.svg)](https://codeclimate.com/github/andela-uenabulele/document-management-system)
+
 A document management system
---
+++ server/models/users.js
@@ -0,0 +1,75 @@
+import bcrypt from 'bcrypt-nodejs';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('Users', {
+    username: {
+      unique: true,
+      type: DataTypes.STRING
+    },
+    fullNames: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    RoleId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        // // associations defined here
+        // User.belongsTo(models.Role, {
+        //   onDelete: 'CASCADE',
+        //   foreignKey: { allowNull: false }
+        // });
+        //
+        // User.hasMany(models.Document, { foreignKey: 'OwnerId' });
+      }
+    },
+    instanceMethods: {
+      /**
+       * Compare plain password to user's hashed password
+       * @method
+       * @param {String} password
+       * @returns {Boolean} password match
+       */
+      validPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+      },
+
+      /**
+       * Hash user's password
+       * @method
+       * @returns {Void} no return
+       */
+      hashPassword() {
+        this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+      }
+    },
+
+    hooks: {
+      beforeCreate(user) {
+        user.hashPassword();
+      },
+
+      beforeUpdate(user) {
+        if (user._changed.password) {
+          user.hashPassword();
+        }
+      }
+    }
+  });
+  return User;
+};
