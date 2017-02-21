4695d9ff0e5e228198fe34172e85f33ab37ee3ec
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2017-02-21 12:16:50 +0100

    test(documents): implement test for document management feature [Starts #137645733]

---
+++ server/test/server/testHelper.js
@@ -0,0 +1,46 @@
+import faker from 'faker';
+
+module.exports = {
+  user: () => ({
+    username: faker.internet.userName(),
+    fullNames: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    RoleId: 2 }),
+  admin: () => ({
+    username: faker.internet.userName(),
+    fullNames: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    RoleId: 1
+  }),
+  noRoleUser: () => ({
+    username: faker.internet.userName(),
+    fullNames: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  }),
+  document: () => ({
+    title: faker.lorem.word(),
+    content: faker.lorem.paragraph(),
+    permission: 'Public',
+    OwnerId: 1
+  }),
+  documentNoPermission: () => ({
+    title: faker.lorem.word(),
+    content: faker.lorem.paragraph(),
+    OwnerId: 1
+  }),
+  documentWithArg: (permission, OwnerId) => ({
+    title: faker.lorem.word(),
+    content: faker.lorem.paragraph(),
+    permission,
+    OwnerId
+  }),
+  defaultRole: () => ({
+    title: faker.lorem.word(),
+    read: true,
+    write: false,
+    delete: false
+  })
+};
