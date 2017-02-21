6e7e00bd2d7046bb93dd2767953aedcacf3c46da
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2017-02-21 18:13:44 +0100

    refactor(app): refactor codebase [Finishes #140309307]

--- server/config/app.js
+++ server/config/app.js
@@ -1,7 +1,11 @@
+/* eslint-disable global-require */
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-import routes from '../routes/index';
+import webpack from 'webpack';
+import path from 'path';
+import Routes from '../Routes/index';
+import config from '../../webpack.config.dev';
 
 const app = express();
 const router = express.Router();
@@ -13,10 +17,26 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const compiler = webpack(config);
+const env = process.env.NODE_ENV || 'development';
+
+// bundle bundle frontend using webpack
+if (env !== 'test') {
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+}
+app.use(require('webpack-hot-middleware')(compiler));
+
 // using express router for routes
-routes(router);
+Routes(router);
 
 // prefix /api for all routes
 app.use('/api', router);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/index.html'));
+});
 
 export default app;
---
+++ server/controller/UsersCtrl.js
@@ -0,0 +1,136 @@
+import jwt from 'jsonwebtoken';
+import db from '../models';
+
+const secret = process.env.SECRET || 'just another open secret';
+
+const userDetails = newUser => ({
+  id: newUser.id,
+  username: newUser.username,
+  fullNames: newUser.fullNames,
+  email: newUser.email,
+  RoleId: newUser.RoleId,
+  createdAt: newUser.createdAt,
+  updatedAt: newUser.updatedAt
+});
+
+const UsersCtrl = {
+  signUp(req, res) {
+    db.Users.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+      .then((userExist) => {
+        if (userExist) {
+          return res.status(201)
+            .send({
+              message: `${req.body.email} already exists`
+            });
+        }
+        db.Users.create(req.body)
+            .then((newUser) => {
+              const token = jwt.sign({
+                UserId: newUser.id,
+                RoleId: newUser.RoleId
+              }, secret, {
+                expiresIn: '2 days'
+              });
+              const user = userDetails(newUser);
+              res.status(201)
+                .send({
+                  user,
+                  token,
+                  expiresIn: '2 days'
+                });
+            })
+            .catch((err) => {
+              res.send({
+                message: 'Error occured creating user',
+                details: err
+              });
+            });
+      })
+      .catch((err) => {
+        res.send({ error: {
+          message: 'Error occured checking existing user',
+          details: err }
+        });
+      });
+  },
+  login(req, res) {
+    db.Users.findOne({
+      where: {
+        $or: [{ email: req.body.username },
+          { username: req.body.username }]
+      }
+    }).then((user) => {
+      if (user && user.validPassword(req.body.password)) {
+        const token = jwt.sign({
+          UserId: user.id,
+          RoleId: user.RoleId
+        }, secret, { expiresIn: '2 days' });
+        res.send({ user, token, expiresIn: '2 days' });
+      } else {
+        res.status(401)
+            .send({ message: 'Authentication failed!' });
+      }
+    }).catch(() => {
+    });
+  },
+  logout(req, res) {
+    res.send({ message: `User with id:${req.decoded.UserId} logged out` });
+  },
+  allUsers(req, res) {
+    db.Users.findAll({ fields: [
+      'id',
+      'username',
+      'fullName',
+      'email',
+      'RoleId',
+      'createdAt',
+      'updatedAt'
+    ] })
+      .then((usersList) => {
+        res.send(usersList);
+      });
+  },
+  getUser(req, res) {
+    db.Users.findOne({
+      where: {
+        $or: [{ email: req.params.id },
+          { username: req.params.id }]
+      }
+    }).then((user) => {
+      if (!user) {
+        return res.status(401)
+        .send({ mesage: `User ${res.param.username} cannot be found` });
+      }
+      res.send(user);
+    });
+  },
+  updateUser(req, res) {
+    db.Users.find({ where: {
+      id: req.params.id } })
+      .then((user) => {
+        user.update(req.body)
+      .then((updatedUser) => {
+        res.send({ message: `${req.params.id} updated`,
+          data: userDetails(updatedUser)
+        });
+      });
+      });
+  },
+  deleteUser(req, res) {
+    db.Users.find({ where: {
+      id: req.params.id } })
+      .then((user) => {
+        user.destroy()
+      .then(() => {
+        res.send(`${req.params.id} has been deleted`);
+      });
+      });
+  }
+
+};
+
+export default UsersCtrl;
